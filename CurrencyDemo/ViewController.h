a2830b2d36272add10d5821b8c22ca2e438b894f
Tony <284096905@qq.com>
2016-03-15 15:24:33 +0800

    Initial Commit

---
+++ CurrencyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyDemo
+//
+//  Created by Tony on 16/3/15.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CurrencyDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyDemo
+//
+//  Created by Tony on 16/3/15.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
